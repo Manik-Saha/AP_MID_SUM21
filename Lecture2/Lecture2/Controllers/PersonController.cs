d6222e90a93e517fbe2f060fe7bea82d7fc8deb2
Manik-Saha <maniksaha.00.mailme@gmail.com>
2021-06-15 20:34:11 +0600

    lecture 2

---
+++ Lecture2/Lecture2/Controllers/PersonController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lecture2.Models;
+
+namespace Lecture2.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            ViewBag.Name = "Manik Saha";
+            TempData["University"] = "AIUB";
+            //return View();
+            return RedirectToAction("Details");
+        }
+
+        /*public ActionResult Details()
+        {
+            ViewData["Name"] = "Manik Saha";
+            return View();
+        }*/
+
+        public ActionResult Details()
+        {
+            Person p = new Person();
+            p.Name = "Manik Saha";
+            p.Password = "113123";
+            p.UserName = "manik_saha";
+
+            return View(p);
+        }
+    }
+}
---
+++ Lecture2/Lecture2/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lecture2.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+}

53e5f159041b5b8e6999a8ce739cc070f3d89749
Manik-Saha <maniksaha.00.mailme@gmail.com>
2021-07-03 12:01:56 +0600

    DB first

---
+++ Lecture8/Lecture8/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Lecture8.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lecture8.Controllers
+{
+    public class ProductController : Controller
+    {
+        PMSEntities context = new PMSEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            var products = context.Products.ToList();
+            return View(products);
+        }
+    }
+}
